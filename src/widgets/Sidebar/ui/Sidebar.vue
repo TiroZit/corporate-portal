d00c68bfbbe1dd43a88cfc60ef041bf67b1b6dec
Nikita Avdeev <nikus.master@gmail.com>
2024-03-29 22:42:33 +0500

    feat(SideBar): добавил аватар

--- src/widgets/Sidebar/ui/Sidebar.vue
+++ src/widgets/Sidebar/ui/Sidebar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from '~/shared/ui/avatar';
 import { Logo, LogoMini } from '~/shared/ui/logo';
 
 const miniMode = ref(true);
@@ -9,17 +10,31 @@ const miniMode = ref(true);
 		:class="{ 'w-48': !miniMode, 'w-16': miniMode }"
 		class="transition-width border-e bg-card p-4 text-card-foreground shadow transition-[width]"
 	>
-		<nav class="flex flex-col gap-6" :data-state="miniMode ? 'collapsed' : 'expanded'">
-			<Logo v-show="!miniMode" @click="miniMode = !miniMode" />
-			<LogoMini v-show="miniMode" @click="miniMode = !miniMode" />
+		<nav class="group flex h-full flex-col gap-6" :data-state="miniMode ? 'collapsed' : 'expanded'">
+			<!-- <Logo @click="miniMode = !miniMode" /> -->
+			<LogoMini @click="miniMode = !miniMode" />
 
 			<ul>
 				<li>
 					<NuxtLink to="/" class="flex items-center gap-2">
-						<Icon name="uil:github" size="32" />
-						<span class="text-sm">
+						<Icon name="uil:github" size="32" class="shrink-0" />
+						<span class="text-sm group-data-[state=collapsed]:hidden">
 							Home
 						</span>
+					</NuxtLink>
+				</li>
+			</ul>
+
+			<ul class="mt-auto">
+				<li>
+					<NuxtLink to="/settings">
+						<Avatar>
+							<AvatarImage
+								src="https://github.com/shadcn.png"
+								alt="Shadcn"
+							/>
+							<AvatarFallback>SC</AvatarFallback>
+						</Avatar>
 					</NuxtLink>
 				</li>
 			</ul>
