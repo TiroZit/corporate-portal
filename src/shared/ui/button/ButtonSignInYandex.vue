b04aedd3b172c2863f079b37afaf46bfcea7b9d0
Nikita Avdeev <nikus.master@gmail.com>
2024-06-05 20:14:44 +0500

    fix: img

--- src/shared/ui/button/ButtonSignInYandex.vue
+++ src/shared/ui/button/ButtonSignInYandex.vue
@@ -1,5 +1,5 @@
 <template>
 	<button class="mt-3 w-full">
-		<NuxtImg class="size-full" src="/img/sign-in-yandex.svg" />
+		<img class="size-full" src="/img/sign-in-yandex.svg">
 	</button>
 </template>
--- src/shared/ui/logo/LogoMini.vue
+++ src/shared/ui/logo/LogoMini.vue
@@ -1,5 +1,5 @@
 <template>
 	<NuxtLink to="/">
-		<NuxtImg src="/img/logo-mini.svg" width="32" />
+		<img src="/img/logo-mini.svg" alt="logo" width="32">
 	</NuxtLink>
 </template>
