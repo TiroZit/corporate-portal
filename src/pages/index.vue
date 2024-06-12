21b79b04282533dd0ac347098e0c6bd89b174f8b
Nikita Avdeev <nikus.master@gmail.com>
2024-06-12 19:51:30 +0500

    fixs

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,5 +3,10 @@
 </script>
 
 <template>
-	<div />
+	<h1 class="text-center text-6xl font-bold">
+		Приветствую!
+	</h1>
+	<p class="mt-4 text-center text-xl">
+		Добро пожаловать на корпоративный портал.
+	</p>
 </template>
